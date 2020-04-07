70ec43822ec760a78641fec9bc577c489d8ee272
liviayurike <kliviayurike@gmail.com>
2020-04-07 08:56:54 +0700

    Jobsheet 9 Pengantar dan Instalasi Laravel

---
+++ laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Profil Saya</title>
+</head>
+
+<body>
+    <h1>Profil Saya</h1>
+    <p>Perkenalkan Nama saya Livia Yurike Khuril Maula, saya seorang Mahasiswi.</p>
+</body>
+
+</html>
